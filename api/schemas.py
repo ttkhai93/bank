3511f77dd591381d94d45a76d79e5b069a7aa9fb
Kai <ttkhai93@gmail.com>
2025-04-10 20:16:58 +0700

    Standardize error response

--- api/schemas.py
+++ api/schemas.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel, Field
+from typing import Any
+from enum import StrEnum
+
+from pydantic import BaseModel, Field, ConfigDict
 
 
 class CommonQueryParams(BaseModel):
@@ -6,4 +9,15 @@ class CommonQueryParams(BaseModel):
     limit: int = Field(default=100, ge=0, le=100)
     order_by: str = None
 
-    model_config = {"extra": "forbid"}
+    model_config = ConfigDict(extra="forbid")
+
+
+class JSONResponseBodyStatus(StrEnum):
+    SUCCESS = "success"
+    ERROR = "error"
+
+
+class JSONResponseBody(BaseModel):
+    status: JSONResponseBodyStatus
+    data: dict[str, Any] = None  # Required If status = "success"
+    message: str = None  # Required If status = "error"
