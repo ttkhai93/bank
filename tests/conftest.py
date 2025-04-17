2a3bfbfdfe5f17bf6c7b8c59950e4ed861b3e152
Kai <ttkhai93@gmail.com>
2025-04-17 13:32:01 +0700

    Refactor conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,13 @@
 import os
 import asyncio
 
+from asyncpg import connect, Connection
 from pytest import fixture
 from httpx import AsyncClient, ASGITransport
-from sqlalchemy import text
 from alembic import command
 from alembic.config import Config
 
 from core.models.base import metadata
-from core.db import engine, transaction
 from settings import db_settings
 from main import app
 from .utils import working_directory
@@ -19,27 +18,14 @@ def postgres_url(docker_services, docker_ip):
     host_port = docker_services.port_for("postgres", 5432)
     url = f"postgresql+asyncpg://postgres:postgres@{docker_ip}:{host_port}/postgres"
 
-    def check_connection() -> bool:
+    def check_connection() -> Connection | None:
         try:
-
-            async def check() -> bool:
-                try:
-                    engine.create(url)
-                    return bool(await transaction.execute(text("SELECT 1;")))
-                finally:
-                    await engine.dispose()
-
-            return asyncio.run(check())
-        except Exception:
-            return False
+            return asyncio.run(connect(url.replace("+asyncpg", "")))
+        except ConnectionResetError:
+            return None
 
     docker_services.wait_until_responsive(check=check_connection, timeout=5, pause=0.5)
     return url
-
-
-@fixture(scope="session", autouse=True)
-def test_settings(postgres_url):
-    db_settings.DATABASE_URL = postgres_url
 
 
 @fixture(scope="session", autouse=True)
@@ -50,22 +36,25 @@ def work_in_project_root():
 
 
 @fixture(scope="session", autouse=True)
-def apply_migrations(test_settings, work_in_project_root):
+def apply_migrations(postgres_url, work_in_project_root):
     """Apply migrations at beginning of test session"""
+    db_settings.DATABASE_URL = postgres_url
     config = Config("alembic.ini")
     command.upgrade(config, "head")
 
 
-async def reset_db():
+@fixture(autouse=True)
+async def reset_db(postgres_url: str):
     """Reset database after each test"""
     tables = ", ".join(metadata.tables.keys())
     sql = f"TRUNCATE TABLE {tables} CASCADE"
-    await transaction.execute(text(sql))
+
+    connection: Connection = await connect(postgres_url.replace("+asyncpg", ""))
+    await connection.execute(sql)
 
 
-@fixture(scope="function")
+@fixture
 async def new_client():
     async with app.router.lifespan_context(app):
         async with AsyncClient(transport=ASGITransport(app), base_url="http://test") as client:
             yield client
-            await reset_db()
