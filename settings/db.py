c21ea7d913935941ffcaeb478001fb6f2cef0e1b
Kai <ttkhai93@gmail.com>
2025-04-17 15:03:06 +0700

    Update default DATABASE_URL to avoid mistaken connect to local db

--- settings/db.py
+++ settings/db.py
@@ -2,7 +2,7 @@ from .base import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/bank"
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/dbname"
     POOL_SIZE: int = 5  # The number of connections is always maintained in the pool, available for immediate reuse
     MAX_OVERFLOW: int = 10  # The number of temporary connections can be open when POOL_SIZE is exhausted
 
